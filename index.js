a947d5e37b88f59571176f8bf0030d35763a46e5
Rodney <rodneypa@LAPTOP-KQJ5G51E.localdomain>
2022-01-23 02:13:46 -0500

    commit tree 3a5987f5c8a9bb27171a498e55a8ab72f213145b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+var myobj = document.getElementById("main");
+myobj.remove();
+
+var newHeader = document.createElement("h1");
+
+newHeader.setAttribute("Id", "victory");
+
+newHeader.textContent = "YOUR-NAME is the champion";
+
+
+
+
